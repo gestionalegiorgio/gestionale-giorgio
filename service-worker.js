ded881a79a088d953fc1f749f8fb35917dd20e45
gestionalegiorgio <cardgif298@gmail.com>
2025-10-24 06:45:50 +0200

    Refactor service worker caching logic and file names

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,26 @@
-const CACHE_NAME = 'gestionale-giorgio-v1';
-const URLS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icone/icona-192.png',
-  './icone/icona-512.png'
+const CACHE_NAME = "gestionale-giorgio-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
-// Install: salva i file base
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
-  );
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
-// Attiva: rimuove cache vecchie
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => 
+    caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-// Fetch: usa cache o rete
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(resp => 
-      resp || fetch(event.request).then(r => {
-        if (!r || r.status !== 200 || r.type !== 'basic') return r;
-        const respClone = r.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, respClone));
-        return r;
-      })
-    )
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
